6a44c8ae84a340d15fa6791eac704a64e7b906fd
jagadish-kanini <jagadish.mangini@kanini.com>
2026-04-13 18:14:09 +0530

    To-Do Project

---
+++ TaskReminder/TaskReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskReminder
+//
+//  Created by Jagadish Mangini on 08/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TaskReminder/TaskReminderTests/TaskReminderTests.swift
@@ -0,0 +1,18 @@
+//
+//  TaskReminderTests.swift
+//  TaskReminderTests
+//
+//  Created by Jagadish Mangini on 10/04/26.
+//
+
+import Testing
+
+struct TaskReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
